62294171ab311abd8145be95ba91e6e88ae0066d
Ratheep <ratheep.ceymplon@jeylabs.com>
2018-07-11 13:06:27 +0530

    build error fixed

--- __tests__/components/shape/HeptagonTest.js
+++ __tests__/components/shape/HeptagonTest.js
@@ -4,6 +4,6 @@ import Heptagon from '../../../app/components/Shape/Heptagon/index';
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
-    const rendered = renderer.create(<Heptagon  properties={{radius : 100}}/>).toJSON();
+    const rendered = renderer.create(<Heptagon  properties={{length : 100}}/>).toJSON();
     expect(rendered).toBeTruthy();
 });
--- app/components/Support/Triangle/propTypes.js
+++ app/components/Support/Triangle/propTypes.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
 
 export default {
-    style: PropTypes.array
+    style: PropTypes.object
 }
